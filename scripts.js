548e6541ba1c6f94b4f6534fc2dee563fdc87179
ButterBean33 <131820939+ButterBean33@users.noreply.github.com>
2023-04-25 23:09:53 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 document.getElementById("reveal-button").addEventListener("click", function() {
     var surprise = document.getElementById("surprise");
-    var sillySong = document.getElementById("silly-song");
+    var sillySong = document.getElementById("Desktop/LamboExp1/silly-song");
 
     surprise.classList.remove("hidden");
     sillySong.play();
